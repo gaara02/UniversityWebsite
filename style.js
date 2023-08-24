1398b11591eef5182b8511f146f21e4ced4a43b4
Fallou Faye <70723345+gaara02@users.noreply.github.com>
2023-08-24 13:06:41 +0000

    Add files via upload

---
+++ style.js
@@ -0,0 +1,11 @@
+
+var menuBox = document.getElementById("nav-links");
+function toggleMenu() {
+    menuBox.style.right = "0";
+
+}
+
+function hideMenu() {
+    menuBox.style.right = "-200px";
+
+    }
